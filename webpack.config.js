80bebc4ec8fd504aee72a7bdd32b51ee398184e3
Asshley <asshleydeanwaynegozar@gmail.com>
2025-06-20 22:16:55 +0800

    initialize webpack configuration file

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/main.js',
+	output: {
+		filename: 'index.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+};
